04b2416bb40354d528e7ff40df5991f7e84458d7
MannepalliDeekshithkumar <mannepallideekshith3@gmail.com>
2026-03-19 22:11:27 +0530

    age caluculator,simple caluculator

---
+++ day3.py
@@ -0,0 +1,25 @@
+#NOW LETS WORK On a simple case study using --> age caluculator
+
+birth_year = int(input("enter your birth year"))
+
+current_year = int(input("enter current year"))
+
+age = current_year - birth_year
+
+print("your age is:",age)
+
+
+
+#Now lets work on simple caluculator
+
+
+
+a=int(input("enter the first number"))
+b=int(input("enter the secomd number"))
+
+
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+      
