813944727119ca082ed572f81f837652215d4d89
MannepalliDeekshithkumar <mannepallideekshith3@gmail.com>
2026-03-21 21:27:33 +0530

    operators with examples

---
+++ day5.py
@@ -0,0 +1,27 @@
+
+# Assigning values to variables
+a = 10
+b = 3
+
+print(f"a = {a}, b = {b}\n")
+
+# Addition
+print(f"Addition (a + b): {a + b}") # Output: 13
+
+# Subtraction
+print(f"Subtraction (a - b): {a - b}") # Output: 7
+
+# Multiplication
+print(f"Multiplication (a * b): {a * b}") # Output: 30
+
+# Division (always returns a float)
+print(f"Division (a / b): {a / b}") # Output: 3.333...
+
+# Modulus (returns the remainder)
+print(f"Modulus (a % b): {a % b}") # Output: 1
+
+# Exponentiation (a to the power of b)
+print(f"Exponentiation (a * b): {a * b}") # Output: 1000
+
+# Floor Division (returns the integer part of the quotient)
+print(f"Floor Division (a // b): {a // b}") # 
